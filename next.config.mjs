13075fd36f5d7cde511e7d740c8e022c4f9ff355
alokrai786 <alokrai.1984@gmail.com>
2026-08-09 19:05:03 +0530

    Enable static export for Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { poweredByHeader: false };
+const nextConfig = {
+  poweredByHeader: false,
+  output: "export"
+};
+
 export default nextConfig;
